cafc928afecf75b782b4a4d17149eba1d60d3fe9
Sukhmandeep Kaur <39960799+Sukhman-Kaur@users.noreply.github.com>
2021-12-19 19:21:19 -0600

    initial commit

---
+++ src/data.js
@@ -0,0 +1,29 @@
+const displayName = 'Sukhmandeep Kaur'
+const bannerDescription = 'Developer'
+const mediumUrl = 'https://api.allorigins.win/raw?url=https://medium.com/feed/@huishun'
+const mediumFilterKeyword = 'technology'
+const email = 'sukhman2807@gmail.com'
+const githubUrl = "https://github.com/Sukhman-Kaur"
+const linkedInUrl = "https://www.linkedin.com/in/sukhmandeep-kaur/"
+const resumePdfTitle = 'Sukhman-Resume.docx'
+
+const projects = [
+    {
+        title: 'Mess Management',
+        description: '',
+        stack: ['Android'],
+        imgName: '',
+        siteUrl: '',
+        codeUrl: '',
+    },
+    {
+        title: 'SK Portfolio',
+        description: 'A portfolio website.',
+        stack: ['React.js'],
+        imgName: 'portfolio.png',
+        siteUrl: '',
+        codeUrl: '',
+    },
+]
+
+export { projects, mediumUrl, mediumFilterKeyword, email, githubUrl, linkedInUrl, displayName, bannerDescription, resumePdfTitle }
