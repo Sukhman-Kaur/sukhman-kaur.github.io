6101bdaf16a02ae7cae3f6e083069b8c45765d9a
Sukhmandeep Kaur <39960799+Sukhman-Kaur@users.noreply.github.com>
2021-12-28 19:12:37 -0600

    Updates

---
+++ precache-manifest.8d4be176ceabf29b00c645dd5a8c17b1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7233cbca260ee7210afd46092b93df0b",
+    "url": "/sukhman-kaur.github.io/index.html"
+  },
+  {
+    "revision": "cc143a39f8d0e36bdc07",
+    "url": "/sukhman-kaur.github.io/static/css/2.e4777ce8.chunk.css"
+  },
+  {
+    "revision": "f82a5f85e3332c58b923",
+    "url": "/sukhman-kaur.github.io/static/css/main.a6e3328e.chunk.css"
+  },
+  {
+    "revision": "cc143a39f8d0e36bdc07",
+    "url": "/sukhman-kaur.github.io/static/js/2.fabcf71a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/sukhman-kaur.github.io/static/js/2.fabcf71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f82a5f85e3332c58b923",
+    "url": "/sukhman-kaur.github.io/static/js/main.cf08b133.chunk.js"
+  },
+  {
+    "revision": "84ffd53211d7301afb41",
+    "url": "/sukhman-kaur.github.io/static/js/runtime-main.7e51f706.js"
+  },
+  {
+    "revision": "a756e0112873e52212b583f157860a26",
+    "url": "/sukhman-kaur.github.io/static/media/developer.a756e011.jpeg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/sukhman-kaur.github.io/precache-manifest.e11b1b590d37112a83fa45e258069461.js"
+  "/sukhman-kaur.github.io/precache-manifest.8d4be176ceabf29b00c645dd5a8c17b1.js"
 );
 
 self.addEventListener('message', (event) => {
