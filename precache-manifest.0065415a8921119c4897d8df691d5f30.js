5703b0d91dc7d0c9335bceedc4b264dc74d69f04
Sukhmandeep Kaur <39960799+Sukhman-Kaur@users.noreply.github.com>
2021-12-28 19:17:38 -0600

    Updates

---
+++ precache-manifest.0065415a8921119c4897d8df691d5f30.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1670e59adac949028d8153715ebc84f8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cc143a39f8d0e36bdc07",
+    "url": "/static/css/2.e4777ce8.chunk.css"
+  },
+  {
+    "revision": "62ea48ca4894db5dc428",
+    "url": "/static/css/main.a9eafdd6.chunk.css"
+  },
+  {
+    "revision": "cc143a39f8d0e36bdc07",
+    "url": "/static/js/2.fabcf71a.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.fabcf71a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "62ea48ca4894db5dc428",
+    "url": "/static/js/main.cf08b133.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "a756e0112873e52212b583f157860a26",
+    "url": "/static/media/developer.a756e011.jpeg"
+  }
+]);
